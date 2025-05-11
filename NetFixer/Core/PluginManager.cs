e99bec56ef466c77c3c1038e344d78d8af3fac98
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2025-05-11 13:41:32 +0300

    basic structure of the plugin system

---
+++ NetFixer/Core/PluginManager.cs
@@ -0,0 +1,16 @@
+﻿using NetFixer.Interfaces;
+using NetFixer.Plugins.Dns;
+
+namespace NetFixer.Core
+{
+    public static class PluginManager
+    {
+        public static List<INetFixPlugin> GetPlugins()
+        {
+            return new List<INetFixPlugin>
+            {
+                new DnsAutoOptimizePlugin()
+            };
+        }
+    }
+}
---
+++ NetFixer/Interfaces/INetFixPlugin.cs
@@ -0,0 +1,8 @@
+﻿namespace NetFixer.Interfaces
+{
+    public interface INetFixPlugin
+    {
+        string Name { get; }
+        Task ExecuteAsync(ILog log, CancellationToken token);
+    }
+}

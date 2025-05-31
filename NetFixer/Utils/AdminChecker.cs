24fbed4c046d4588df91a944bf62dd3efb1b13f4
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2025-05-31 17:27:31 +0300

    Implemented check for running as administrator

---
+++ NetFixer/Utils/AdminChecker.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Principal;
+
+namespace NetFixer.Utils
+{
+    public static class AdminChecker
+    {
+        public static bool IsRunAsAdmin()
+        {
+            using var identity = WindowsIdentity.GetCurrent();
+
+            return new WindowsPrincipal(identity)
+                .IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}

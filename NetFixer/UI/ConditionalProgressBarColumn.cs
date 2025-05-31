f6d4e46c649a9be03b47872c558d1fcd6195837f
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2025-05-31 17:24:58 +0300

    Added custom progress columns with display conditions

---
+++ NetFixer/UI/ConditionalProgressBarColumn.cs
@@ -0,0 +1,29 @@
+﻿using Spectre.Console;
+using Spectre.Console.Rendering;
+
+namespace NetFixer.UI
+{
+    public class ConditionalProgressBarColumn : ProgressColumn
+    {
+        public override IRenderable Render(RenderOptions options, ProgressTask task, TimeSpan deltaTime)
+        {
+            if (task.Description.Contains("HIDDEN_TASK"))
+                return new Text("");
+
+            return new ProgressBarColumn().Render(options, task, deltaTime);
+        }
+    }
+
+    public class ConditionalPercentageColumn : ProgressColumn
+    {
+        public override IRenderable Render(RenderOptions options, ProgressTask task, TimeSpan deltaTime)
+        {
+            if (task.Description.Contains("HIDDEN_TASK"))
+                return new Text("");
+
+            return new PercentageColumn().Render(options, task, deltaTime);
+        }
+    }
+
+
+}
